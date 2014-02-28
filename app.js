fe32707cc0d550c3e0533aaa9f3000c92832892a
Raphael <raphael_rong@Hotmail>
2014-02-28 21:06:12 +0800

    新增mongoose操作数据库的简单实现
    user.js
    1.新建Person表schema
    
    app.js
    1.数据库建立连接后向数据库中插入一条数据数据（已注释，第一次运行时请取消注释，不然数据库中没数据）
    2.更改/users接口，查询数据库中Person表中的某一条数据

--- app.js
+++ app.js
@@ -9,6 +9,29 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+var controller = require('./public/javascripts/controller/user');
+
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+   console.log('connection success');
+
+//    var Person = mongoose.model('Person');
+//    var person = new Person({name:{first:'Raphael', last:'Rong'}, email:'raphael_rong@hotmail', alive:true});
+//    console.log(person);
+//    person.save(function (err) {
+//        if (err) return handleError(err);
+//
+//    })
+    
+});
+
+
+
+
 var app = express();
 
 // all environments
@@ -29,7 +52,17 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/users', function(req, res){
+
+    var Person = mongoose.model('Person');
+    Person.find(function (err, doc) {
+        if (err) return handleError(err);
+        for (var aDoc in doc) {
+            console.log(doc); // { name: 'mongodb.org', _id: '50341373e894ad16347efe12' }
+            res.send(doc);
+        }
+    })
+});
 
 app.get('/stooges/*?', function(req, res){
 });
